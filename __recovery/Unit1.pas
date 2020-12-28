4004fad58423c41a32fe12178b2953793f9bc58e
Murillo Master <murillojndem@gmail.com>
2020-12-28 17:09:03 -0300

    iniciado link entre crud e banco de dados

---
+++ __recovery/Unit1.pas
@@ -0,0 +1,42 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, FireDAC.Stan.Intf,
+  FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf,
+  FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys,
+  FireDAC.VCLUI.Wait, Data.DB, FireDAC.Comp.Client;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    txt_ID: TEdit;
+    txt_nome: TEdit;
+    txt_email: TEdit;
+    txt_telefone: TEdit;
+    Memo: TMemo;
+    FDConnection1: TFDConnection;
+    FDContatos: TFDTable;
+    DataSource1: TDataSource;
+    btn_NOVO: TButton;
+    btn_SALVAR: TButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
